2419a3e92e95331ca2eb7427416d1d20ef41f3b8
Opilane TTHK <opilane@tthk.ee>
2021-10-15 15:48:04 +0300

    Add project files.

---
+++ palindrom.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isetoo_Kordamine
+{
+    class palindrom
+    {
+ /*       static void Main(string[] args)
+        {
+            Console.WriteLine("Sisestage tekst: ");
+            string str = Console.ReadLine();
+            char[] symbol = { ' ', ',', '.', '!', '?' };
+            for (; ; )
+            {
+                int offset = str.IndexOfAny(symbol);
+                if (offset == -1) break;
+                string st1 = str.Substring(0, offset);
+                string st2 = str.Substring(offset + 1);
+                str = string.Concat(st1, st2);
+            }
+            Char[] s = str.ToCharArray();
+            Char[] s1 = new char[str.Length];
+            int index = s.Length - 1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                s1[i] = s[index--];
+            }
+            string str1 = new string(s);
+            string str2 = new string(s1);
+            if (string.Compare(str1, str2, true) == 0)
+            {
+                Console.Write("String - palindroom");
+            }
+            else Console.Write("String ei ole palindroom");
+            Console.ReadLine();
+        }
+    }
+}*/
---
+++ u3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isetoo_Kordamine
+{
+    class u3
+    {
+        public static void Main(string[] args)
+        {
+            int arv;
+            int[] arvud = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Sisesta {0} arv:", i + 1);
+                arv = int.Parse(Console.ReadLine());
+                arvud[i] = arv;
+            }
+            Array.Sort(arvud);
+            foreach (var a in arvud)
+            {
+                Console.Write(a);
+            }
+            int arv4 = 0;
+            Array.Reverse(arvud);
+            foreach (var a in arvud)
+            {
+                arv4 = arv4 * 10 + a;
+
+            }
+            Console.Write(arv4);
+            Console.ReadLine();
+        }
+        
+    }
+}
